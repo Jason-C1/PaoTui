691757aa1f35e9dec42b0642cedcf4b0f22067b8
1045290202 <1045290202@qq.com>
2018-03-21 16:36:16 +0800

    sjk
    1.加入评价页和订单跟踪页
    2.加入一个全局js
    3.加入几张图片

---
+++ Client/js/statusBar.js
@@ -0,0 +1,8 @@
+document.addEventListener('plusready', function() {
+	plus.navigator.setStatusBarBackground("#f7f7f7");
+	plus.navigator.setStatusBarStyle("dark");
+}, false);
+
+mui.init({
+	swipeBack: true
+});
