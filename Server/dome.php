94ac5e7e28d68faf72a8f263639c6ce3dac48d64
Jason <547490618@qq.com>
2018-03-23 15:37:21 +0800

    修改index.js逻辑，添加vue和dome.php
    修改index.js逻辑，添加vue和dome.php

---
+++ Server/dome.php
@@ -0,0 +1,68 @@
+﻿<?php
+echo '
+[{
+	"number": "1",
+	"category": "校园帮帮",
+	"content": "去垃圾街16号商铺买一碗鸭血粉丝，不要辣加香菜加醋",
+	"title": "去垃圾街卖一碗鸭血粉丝",
+	"dateTime": "2018-03-21 17:00:00",
+	"deadline": "2018-03-21 18:00:00",
+	"userAddress": "立业园3#001",
+	"ortherAddress": "垃圾街16号",
+	"advanceCharge": "15",
+	"userNumber": "12",
+	"state": "0",
+	"pictureOne": null,
+	"pictureTwo": null,
+	"pictureThree": null,
+	"runnerNumber": "0"
+}, {
+	"number": "2",
+	"category": "卖家直达",
+	"content": "送一份牛肉面",
+	"title": "送一份牛肉面",
+	"dateTime": "2018-03-21 11:00:00",
+	"deadline": "2018-03-21 13:00:00",
+	"userAddress": "一三街18号",
+	"ortherAddress": "立业园3#002",
+	"advanceCharge": "3",
+	"userNumber": "1",
+	"state": "1",
+	"pictureOne": null,
+	"pictureTwo": null,
+	"pictureThree": null,
+	"runnerNumber": "1"
+}, {
+	"number": "3",
+	"category": "同城配送",
+	"content": "买一束鲜花送到开心公司，168玫瑰套装。给小小花",
+	"title": "买一束鲜花送到开心公司",
+	"dateTime": "2018-03-23 00:00:00",
+	"deadline": "2018-03-23 14:00:00",
+	"userAddress": "花街178号“魅力鲜花”店",
+	"ortherAddress": "开心公司",
+	"advanceCharge": "180",
+	"userNumber": "3",
+	"state": "2",
+	"pictureOne": null,
+	"pictureTwo": null,
+	"pictureThree": null,
+	"runnerNumber": "2"
+}, {
+	"number": "4",
+	"category": "生鲜蔬果",
+	"content": "去物美超市买一斤红富士苹果",
+	"title": "去物美超市买一斤红富士苹果",
+	"dateTime": "2018-03-24 11:00:00",
+	"deadline": "2018-03-24 15:00:00",
+	"userAddress": "立业园3#005",
+	"ortherAddress": "滨江浦沿路物美超市",
+	"advanceCharge": "30",
+	"userNumber": "3",
+	"state": "3",
+	"pictureOne": null,
+	"pictureTwo": null,
+	"pictureThree": null,
+	"runnerNumber": "0"
+}]
+';
