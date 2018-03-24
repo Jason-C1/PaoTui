a84f394f255cfebef6744aac1465b32d12ebc114
Jason <547490618@qq.com>
2018-03-25 00:22:24 +0800

    首页样式修改
    首页可滚动可下拉上载，美化选项卡，选项卡可水平滚动

--- Client/js/index.js
+++ Client/js/index.js
@@ -1,77 +1,18 @@
-(function() {
-	var theurl = "http://10.103.105.55";
-	var lastId = '',
-		minId = ''; //最新新闻的id 
-	mui.init({
-		swipeBack: true, //启用右滑关闭功能
-		pullRefresh: {
-			container: '#dd',
-			down: {
-				style: 'circle', //必选，下拉刷新样式，目前支持原生5+ ‘circle’ 样式
-				auto: true, //可选,默认false.首次加载自动上拉刷新一次
-				callback: pulldownRefresh,
-			},
-			up: {
-				contentrefresh: '正在加载...',
-				contentnomore: '没有更多数据了',
-				callback: pullupRefresh,
-			}
-		}
+
+(function($) {
+	$.init({
+		swipeBack: false, //关闭右滑关闭功能
 	});
-	/**
-	 *  下拉刷新获取最新列表 
-	 */
-	function pulldownRefresh() {
-
-		if(window.plus && plus.networkinfo.getCurrentType() === plus.networkinfo.CONNECTION_NONE) {
-			plus.nativeUI.toast('似乎已断开与互联网的连接', {
-				verticalAlign: 'top'
-			});
-			return;
-		}
-		if(lastId) { //说明已有数据，目前处于下拉刷新，增加时间戳，触发服务端立即刷新，返回最新数据
-			data.lastId = lastId;
-			data.time = new Date().getTime() + "";
-		}
-		mui.getJSON(theurl + "/index.php", {}, function(list) {
-			mui('#dd').pullRefresh().endPulldownToRefresh();
-			var list0 = list.C0;
-			if(list0 && list0.length > 0) {
-				lastId = list0[0].id; //保存最新消息的id，方便下拉刷新时使用
-				if(!minId) { //首次拉取列表时保存最后一条消息的id，方便上拉加载时使用
-					minId = list0[list0.length - 1].id;
-				}
-
-				list0.forEach(function(item) {
-					order.c0.push({
-						uId: item.uId,
-						title: item.title,
-						price: item.price,
-						oFrom: item.oFrom,
-						oTo: item.oTo,
-						oId: item.oId,
-						deadline: item.deadline,
-						dateTime: item.dateTime,
-					});
-				});
-			}
-		});
-
-		//请求最新列表信息流
-		//		mui.getJSON("http://spider.dcloud.net.cn/api/news", data, function(rsp) {
-		//			mui('#list').pullRefresh().endPulldownToRefresh();
-		//		if(rsp && rsp.length > 0) {
-		//			lastId = rsp[0].id; //保存最新消息的id，方便下拉刷新时使用
-		//
-		//			if(!minId) { //首次拉取列表时保存最后一条消息的id，方便上拉加载时使用
-		//				minId = rsp[rsp.length - 1].id;
-		//			}
-		//			news.items = convert(rsp).concat(news.items);
-		//		}
-		//		});
-
-	}
-
+	var hostUrl = "http://10.103.105.55";
+		var lastId = '',
+			minId = ''; //最新新闻的id 
+	//阻尼系数
+	var deceleration = mui.os.ios ? 0.003 : 0.0009;
+	$('.mui-scroll-wrapper').scroll({
+		bounce: false,
+		indicators: true, //是否显示滚动条
+		deceleration: deceleration
+	});
 	mui.plusReady(function() {
 		/**
 		 * 获取本地存储中launchFlag的值
@@ -85,13 +26,6 @@
 		}
 		var self = plus.webview.currentWebview(),
 			leftPos = Math.ceil((window.innerWidth - 60) / 2); // 设置凸起大图标为水平居中
-
-		var deceleration = mui.os.ios ? 0.003 : 0.0009;
-		mui('.mui-scroll-wrapper').scroll({
-			bounce: false,
-			indicators: true, //是否显示滚动条
-			deceleration: deceleration
-		});
 		/**
 		 * drawNativeIcon 绘制带边框的半圆，
 		 * 实现原理：
@@ -211,7 +145,6 @@
 			if(targetPage == activePage) {
 				return;
 			}
-
 			//底部选项卡切换
 			util.toggleNview(currIndex);
 			// 子页面切换
@@ -221,60 +154,43 @@
 		});
 
 	});
-
-	var order = new Vue({
-		el: '#dd',
-		data: {
-			c0: [],
-			c1: [],
-			c2: [],
-			c3: [],
-			c4: [],
-		}
-
-	})
-
-	function pullupRefresh() {
-		//		var data = {
-		//			column: "id,post_id,title,author_name,cover,published_at" //需要的字段名
-		//		};
-		//
-		//		if(minId) { //说明已有数据，目前处于上拉加载，传递当前minId 返回历史数据
-		//			data.minId = minId;
-		//			data.time = new Date().getTime() + "";
-		//			data.pageSize = 10;
-		//		}
-		//		//请求历史列表信息流
-		//		//				mui.getJSON("http://spider.dcloud.net.cn/api/news", data, function(rsp) {
-		//		//					mui('#list').pullRefresh().endPullupToRefresh();
-		//		//					if(rsp && rsp.length > 0) {
-		//		//						minId = rsp[rsp.length - 1].id; //保存最后一条消息的id，上拉加载时使用
-		//		//						news.items = news.items.concat(convert(rsp));
-		//		//					}
-		//		//				});
-		//		mui.getJSON("http://172.19.24.199/index.php", data, function(list) {
-		//			list.C0.forEach(function(item) {
-		//				order.c0.push({
-		//					uId: item.uId,
-		//					title: item.title,
-		//					price: item.price,
-		//					oFrom: item.oFrom,
-		//					oTo: item.oTo,
-		//					oId: item.oId,
-		//					deadline: item.deadline,
-		//					dateTime: item.dateTime,
-		//				});
-		//			});
-		//		});
-	}
-
-	mui('.mui-scroll-wrapper').scroll({
-		scrollY: true, //是否竖向滚动
-		scrollX: false, //是否横向滚动
-		startX: 0, //初始化时滚动至x
-		startY: 0, //初始化时滚动至y
-		indicators: true, //是否显示滚动条
-		deceleration: 0.0006, //阻尼系数,系数越小滑动越灵敏
-		bounce: true, //是否启用回弹
+	$.ready(function() {
+		//循环初始化所有下拉刷新，上拉加载。
+		$.each(document.querySelectorAll('.mui-slider-group .mui-scroll'), function(index, pullRefreshEl) {
+			$(pullRefreshEl).pullToRefresh({
+				down: {
+					callback: function() {
+						var self = this;
+						setTimeout(function() {
+							var ul = self.element.querySelector('.mui-table-view');
+							ul.insertBefore(createFragment(ul, index, 10, true), ul.firstChild);
+							self.endPullDownToRefresh();
+						}, 1000);
+					}
+				},
+				up: {
+					callback: function() {
+						var self = this;
+						setTimeout(function() {
+							var ul = self.element.querySelector('.mui-table-view');
+							ul.appendChild(createFragment(ul, index, 5));
+							self.endPullUpToRefresh();
+						}, 1000);
+					}
+				}
+			});
+		});
+		var createFragment = function(ul, index, count, reverse) {
+			var length = ul.querySelectorAll('li').length;
+			var fragment = document.createDocumentFragment();
+			var li;
+			for(var i = 0; i < count; i++) {
+				li = document.createElement('li');
+				li.className = 'mui-table-view-cell';
+				li.innerHTML = '第' + (index + 1) + '个选项卡子项-' + (length + (reverse ? (count - i) : (i + 1)));
+				fragment.appendChild(li);
+			}
+			return fragment;
+		};
 	});
-})();
+})(mui);
